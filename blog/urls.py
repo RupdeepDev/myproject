a3735d7d9b1cf378a8d4d613a27f57585b2f0e7b
oleh10hoba <oleg.10@urk.net>
2019-10-23 15:59:45 +0200

    additional code

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('posts', views.post_detail, name='post_detail'),
     path('post/<int:pk>/', views.post_zoom, name='post_zoom'),
     path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/edit/', views.post_edit, name='post_edit'),
 ]
